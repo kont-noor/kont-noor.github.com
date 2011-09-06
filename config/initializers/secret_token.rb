29fe59d0dec45ed462d79529b4bc7a0fa12c7f79
Nickolay <devmarkup@gmail.com>
2011-09-06 11:20:43 +0300

    just rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KontNoor::Application.config.secret_token = 'be8de0d66e498cc272929b8d67ba0fa81aef2b530ffc325119038509677a711726e43648ba1babcb77122402206a0025982c6982f8eca6949ad6317346e6a9f4'
